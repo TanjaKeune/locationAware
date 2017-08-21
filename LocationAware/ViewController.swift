96060c081f40109d333e30e1f5a8285fbd80091c
Tanja Keune <tanjakeune@gmail.com>
2017-08-21 09:23:31 -0400

    gets your location, speed, altitude and closes address

--- LocationAware/ViewController.swift
+++ LocationAware/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var courseLable: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    @IBOutlet weak var altitudeLabel: UILabel!
+    
+    @IBOutlet weak var addressLabel: UILabel!
+    
+    
+    
+    var manager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +39,49 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(locations)
+        let location = locations[0]
+        self.latitudeLabel.text = String(location.coordinate.latitude)
+        self.longitudeLabel.text = String(location.coordinate.longitude)
+        self.courseLable.text = String(location.course)
+        self.speedLabel.text = String(location.speed)
+        self.altitudeLabel.text = String(location.altitude)
+        
+        CLGeocoder().reverseGeocodeLocation(location) { (placemarks, error) in
+            
+            if error != nil {
+                print(error)
+            } else {
+                if let placemark = placemarks?[0] {
+                    
+                    var address = ""
+                    if placemark.subThoroughfare != nil {
+                        
+                        address += placemark.subThoroughfare! + " "
+                    }
+                    if placemark.thoroughfare != nil {
+                        address += placemark.thoroughfare! + "\n"
+                    }
+                    if placemark.subLocality != nil {
+                        address += placemark.subLocality! + " "
+                    }
+                    if placemark.subAdministrativeArea != nil {
+                        address += placemark.subAdministrativeArea! + " "
+                    }
+                    if placemark.postalCode != nil {
+                        address += placemark.postalCode! + " "
+                    }
+                    if placemark.country != nil {
+                        address += placemark.country!
+                    }
+                    
+                    self.addressLabel.text = address
+                    
+                }
+            }
+            
+        }
+    }
 }
 
